d78432ccfa0a5ec77f4c88fab8dd134bcc2a44dc
ievaleros <ievaleros@gmail.com>
2022-07-27 09:13:18 -0500

    Creacion usuario

--- authApp/admin.py
+++ authApp/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from authApp.models.Producto import Producto
 from authApp.models.proveedor import Proveedor
 from authApp.models.Categoria import Categoria
+from authApp.models.usuario import Usuario
 
 # Register your models here.
 admin.site.register(Proveedor)
 admin.site.register(Categoria)
 admin.site.register(Producto)
+admin.site.register(Usuario)
