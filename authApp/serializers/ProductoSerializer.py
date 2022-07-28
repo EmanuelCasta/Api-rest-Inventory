5f723635441ee57d0a2d2f49196949da27fed52f
EmanuelCasta <emanuel.castaneda.cardona@gmail.com>
2022-07-27 20:52:08 -0500

    Creacion de view Producto

--- authApp/serializers/ProductoSerializer.py
+++ authApp/serializers/ProductoSerializer.py
@@ -6,9 +6,19 @@ from authApp.serializers.CategoriaSerializer import CategoriaSerializer
 from authApp.serializers.ProveedorSerializer import ProveedorSerializer
 
 class ProductoSerializer(serializers.ModelSerializer):
-    categoria = Categoria()
-    proveedor = Proveedor()
+    categoria = CategoriaSerializer()
+    proveedor = ProveedorSerializer()
+    idProveedor =  serializers.IntegerField(write_only=True)
+    idCategoria=  serializers.IntegerField(write_only=True)
 
     class Meta:
         model = Producto()
+        ordering = ["-idProducto"]
         fields = ["idProducto","fechaIngreso","precio","descripcion","cantidad","idProveedor","idCategoria"]
+        depth = 1
+    
+    def create(self, validated_data):
+        proveedorInstance = Proveedor.objects.create(**validated_data)
+        categoriarInstance = Categoria.objects.create(**validated_data)
+        productoInstance = Producto.objects.create(idProveedor=proveedorInstance,idCategoria=categoriarInstance, **validated_data)
+        return productoInstance
