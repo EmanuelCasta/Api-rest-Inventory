4895b4f4b21f8fcf7cc294b5097d7d139323ac2c
Astrid Nino <astrid.nio@gmail.com>
2022-08-01 00:44:39 -0500

    add view a tabla empresa

---
+++ authApp/views/EmpresaCreateView.py
@@ -0,0 +1,13 @@
+from rest_framework import status, views
+from rest_framework.response import Response
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from authApp.serializers.EmpresaSerializer import EmpresaSerializer
+
+class EmpresaDetailView(views.APIView):
+
+    def post(self,request,*arg,**kwargs):
+        serializer = EmpresaSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(status=status.HTTP_201_CREATED)
---
+++ authApp/views/EmpresaDetailView.py
@@ -0,0 +1,8 @@
+from django.conf import settings
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework_simplejwt.backends import TokenBackend
+from rest_framework.permissions import IsAuthenticated
+
+from authApp.models.Empresa import Empresa
+from authApp.serializers.EmpresaSerializer import EmpresaSerializer
