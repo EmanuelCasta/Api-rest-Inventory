5f9c47cea6abc86fc779f67fa3f98098c7bda3bd
Astrid Nino <astrid.nio@gmail.com>
2022-08-01 09:44:47 -0500

    intento 2 fix build

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn authProject.wsi
+web: gunicorn authProject.wsgi
--- authProject/settings.py
+++ authProject/settings.py
@@ -151,4 +151,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'authApp.Usuario'
+import django_heroku
+django_heroku.settings(locals())
